a5eaf06bf15203f8bbccf683ffd5b6599a682557
pdro-h0 <pedro.pho792@gmail.com>
2023-10-26 16:56:08 -0300

    estilizando o link ativo no Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,17 @@
-import Link from "next/link";
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const links = [
-  {label: "Dashboard", href:"/"},
-  {label: "Issues", href:"/issues"},
-]
+  { label: "Dashboard", href: "/" },
+  { label: "Issues", href: "/issues" },
+];
 
 const Navbar = () => {
+  const currentPathName = usePathname();
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">LOGO</Link>
@@ -14,9 +19,16 @@ const Navbar = () => {
       <ul className="flex space-x-6">
         {links.map((link) => (
           <li key={link.href}>
-            <Link 
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
-            href={link.href}>{link.label}</Link>
+            <Link
+              className={`${
+                link.href === currentPathName
+                  ? "text-zinc-900"
+                  : "text-zinc-500 "
+              }hover:text-zinc-800 transition-colors`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
           </li>
         ))}
       </ul>
